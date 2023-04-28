ac6eb77996e7653d61579f539c03b508025e4e66
rmso27 <rubenmsoliveira@gmail.com>
2023-04-28 17:04:20 +0100

    added databse methods file

---
+++ app/database.py
@@ -0,0 +1,51 @@
+## IMPORTS ##
+
+# Import modules
+import pymongo
+from pymongo.errors import ServerSelectionTimeoutError
+import configparser
+
+# Read configuration file
+config = configparser.ConfigParser()
+config.read('configs/configs.ini')
+
+# Database object
+class Database(object):
+    mongodb_conn_string = config['database']['MONGODB_CONN_SRTING']
+    DATABASE = None
+
+    # Initiate DB connection
+    @staticmethod
+    def initialize():
+        mongodb = pymongo.MongoClient(Database.mongodb_conn_string)
+        Database.DATABASE = mongodb['wlm']
+
+    # Insert many method
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    # Insert one method
+    @staticmethod
+    def insert_one(collection, data):
+        Database.DATABASE[collection].insert_one(data)
+
+    # Find many method
+    @staticmethod
+    def find(collection, query, projection):
+        return Database.DATABASE[collection].find(query, projection)
+
+    # Find many method
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
+
+    # Delete one method
+    @staticmethod
+    def delete_one(collection, data):
+        Database.DATABASE[collection].delete_one(data)
+
+    # Update one method
+    @staticmethod
+    def update_one(collection, query, data):
+        Database.DATABASE[collection].update_one(query, data)
